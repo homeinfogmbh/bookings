07e7761efda8d294a63ea3f6bed827ead0049108
Richard Neumann <r.neumann@homeinfo.de>
2019-08-19 16:31:42 +0200

    Added handling of rentings.

--- rentallib/wsgi.py
+++ rentallib/wsgi.py
@@ -5,7 +5,7 @@ from flask import request
 from his import CUSTOMER, Application
 from wsgilib import JSON, JSONMessage
 
-from rentallib.orm import Rentable
+from rentallib.orm import Rentable, Renting
 
 
 __all__ = ['APPLICATION']
@@ -16,6 +16,9 @@ NO_SUCH_RENTABLE = JSONMessage('No such rentable.', status=404)
 RENTABLE_ADDED = JSONMessage('The rentable has been added.', status=201)
 RENTABLE_PATCHED = JSONMessage('The rentable has been updated.', status=200)
 RENTABLE_DELETED = JSONMessage('The rentable has been deleted.', status=200)
+NO_SUCH_RENTING = JSONMessage('No such renting.', status=404)
+RENTING_PATCHED = JSONMessage('The renting has been updated.', status=200)
+RENTING_DELETED = JSONMessage('The renting has been deleted.', status=200)
 
 
 def get_rentable(ident):
@@ -28,7 +31,17 @@ def get_rentable(ident):
         raise NO_SUCH_RENTABLE
 
 
-@APPLICATION.route('/rentables', methods=['GET'])
+def get_renting(ident):
+    """Returns the respective renting."""
+
+    try:
+        return Renting.get(
+            (Renting.id == ident) & (Renting.customer == CUSTOMER.id))
+    except Renting.DoesNotExist:
+        raise NO_SUCH_RENTING
+
+
+@APPLICATION.route('/rentable', methods=['GET'])
 def list_rentables():
     """Lists rentables."""
 
@@ -62,3 +75,40 @@ def delete_rentable(ident):
 
     get_rentable(ident).delete_instance()
     return RENTABLE_DELETED
+
+
+@APPLICATION.route('/renting', methods=['GET'])
+def list_rentings():
+    """Lists available rents."""
+
+    return JSON([
+        renting.to_json() for renting in Renting.select().join(Rentable).where(
+            Rentable.customer == CUSTOMER.id)])
+
+
+@APPLICATION.route('/renting/<int:rentable>', methods=['GET'])
+def list_rentings_of_rentable(rentable):
+    """Returns rentings of the respective rentable."""
+
+    return JSON([
+        renting.to_json() for renting in Renting.select().join(Rentable).where(
+            (Rentable.customer == CUSTOMER.id)
+            & (Renting.rentable == rentable))])
+
+
+@APPLICATION.route('/renting/<int:ident>', methods=['PATCH'])
+def patch_renting(ident):
+    """Patches the respective renting."""
+
+    renting = get_renting(ident)
+    renting.patch_json(request.json)
+    renting.save()
+    return RENTING_PATCHED
+
+
+@APPLICATION.route('/renting/<int:ident>', methods=['DELETE'])
+def delete_renting(ident):
+    """Deletes the respective renting."""
+
+    get_renting(ident).delete_instance()
+    return RENTING_DELETED
