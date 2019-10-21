a568ee88397ea22242f3adb784f749b71927ddd5
Richard Neumann <r.neumann@homeinfo.de>
2019-10-21 12:07:45 +0200

    Simplified overlap check.

--- bookings/orm.py
+++ bookings/orm.py
@@ -95,11 +95,8 @@ class Booking(_BookingsModel):
         cls = type(self)
         cond_not_self = cls.id != self.id
         cond_same_bookable = cls.bookable == self.bookable
-        cond_start_within = (cls.start >= self.start) & (cls.start <= self.end)
-        cond_end_within = (cls.end > self.start) & (cls.end < self.end)
-        cond_overspan = (cls.start < self.end) & (cls.end > self.start)
-        cond_conflict = cond_start_within | cond_end_within | cond_overspan
-        select = cond_not_self & cond_same_bookable & cond_conflict
+        cond_overlap = (cls.start <= self.end) & (cls.end >= self.start)
+        select = cond_not_self & cond_same_bookable & cond_overlap
         return cls.select().where(select)
 
     def check_conflicts(self):
